3afac03386cc78039a5350e27e6ae1a026b147af
Rafael <rafael@brinhosa.com.br>
2022-12-13 09:56:59 +0000

    Create osint-script.sh

---
+++ osint-script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Check if a domain was provided
+if [ -z "$1" ]; then
+  echo "Please provide a domain as input"
+  exit 1
+fi
+
+# Set the domain
+domain=$1
+
+# Set the output file
+output="osint-results-$domain.txt"
+
+# Create the output file
+touch $output
+
+# Use subbrute to find subdomains
+echo "Running subbrute for $domain"
+docker run --rm -it m4ll0k/subbrute $domain >> $output
+
+# Use amass to find subdomains
+echo "Running amass for $domain"
+docker run --rm -it amass enum -d $domain >> $output
+
+# Use findomain to find subdomains
+echo "Running findomain for $domain"
+docker run --rm -it findomain -t $domain >> $output
+
+# Use dnsrecon to find DNS records
+echo "Running dnsrecon for $domain"
+docker run --rm -it dnsrecon -d $domain >> $output
+
+# Use theHarvester to find email addresses
+echo "Running theHarvester for $domain"
+docker run --rm -it theharvester -d $domain -b all >> $output
+
+echo "Finished running OSINT tools for $domain"
