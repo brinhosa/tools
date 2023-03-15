78063369852643f0606a1db3ab878dd9e7abf5c5
Rafael <rafael@brinhosa.com.br>
2023-03-15 11:12:10 +0000

    Update osint-script.sh

--- osint-script.sh
+++ osint-script.sh
@@ -17,7 +17,7 @@ touch $output
 
 # Use subbrute to find subdomains
 echo "Running subbrute for $domain"
-docker run --rm -it m4ll0k/subbrute $domain >> $output
+docker run --rm -it trickest/subbrute $domain >> $output
 
 # Use amass to find subdomains
 echo "Running amass for $domain"
@@ -25,14 +25,14 @@ docker run --rm -it caffix/amass enum -d $domain >> $output
 
 # Use findomain to find subdomains
 echo "Running findomain for $domain"
-docker run --rm -it findomain -t $domain >> $output
+docker run --rm -it edu4rdshl/findomain -t $domain >> $output
 
 # Use dnsrecon to find DNS records
 echo "Running dnsrecon for $domain"
-docker run --rm -it dnsrecon -d $domain >> $output
+docker run --rm -it cr0hn/dnsrecon -d $domain >> $output
 
 # Use theHarvester to find email addresses
 echo "Running theHarvester for $domain"
-docker run --rm -it theharvester -d $domain -b all >> $output
+docker run --rm -it simonthomas/theharvester -d $domain -b all >> $output
 
 echo "Finished running OSINT tools for $domain"
